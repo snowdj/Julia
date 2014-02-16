7b4bc78a12e0ae386cad82f8862ead2454f37fc4
DragonflyStats <kobriendublin@gmail.com>
2014-02-16 17:34:43 +0000

    Update WinstonExamples.jl

--- WinstonExamples.jl
+++ WinstonExamples.jl
@@ -35,6 +35,29 @@ yB = x + 5randn(n)
 
 a = Points(x, yA, kind="circle")
 setattr(a, label="a points")
+%-------------------------------------------%
+
+using Winston
+
+p = FramedPlot(
+        title="Title",
+        xlabel="X axis",
+        ylabel="Y axis")
+
+add(p, Histogram(hist(randn(1000))...))
+add(p, PlotLabel(.5, .5, "Histogram", color=0xcc0000))
+
+t1 = Table(1, 2)
+t1[1,1] = p
+t1[1,2] = p
+
+t2 = Table(2, 1)
+t2[1,1] = t1
+t2[2,1] = p
+
+file(t2, "example3.png")
+
+%-------------------------------------------%
 
 b = Points(x, yB)
 setattr(b, label="b points")
